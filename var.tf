e0e763603d096f2dcf3ec552bfff78c086ecee22
windows 10 <dimpulsunayana205@gmail.com>
2023-04-12 14:21:18 +0530

    updated user repo

--- var.tf
+++ var.tf
@@ -2,7 +2,3 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "allow_cidr" {}
 variable "main_vpc" {}
-variable "host_instance_type" {}
-variable "engine_version" {}
-variable "engine_type" {}
-variable "deployment_mode" {}
